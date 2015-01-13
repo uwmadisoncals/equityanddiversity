40caecd3bdb735cafbb3c4b1a9d2991a0faec525
Daniel Dropik <ddropik@wisc.edu>
2015-01-13 15:27:01 -0600

    Changing the output of the function add_slug_body_class so that it is easier to identify in the developer console adding a new scss document in partials for controlling the css of the committee members page

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 function add_slug_body_class( $classes ) {
 global $post;
 if ( isset( $post ) ) {
-$classes[] = $post->post_type . '-' . $post->post_name;
+$classes[] = '*' . $post->post_type . '-' . $post->post_name;
 }
 return $classes;
 }
