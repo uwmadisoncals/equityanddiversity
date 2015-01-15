3227d98c2f596f6a9358986a7beea1f0ae887141
Daniel Dropik <ddropik@wisc.edu>
2015-01-15 13:37:39 -0600

    functions.php: removing the * from the body class slug gulpfile.js: comment out minifycss because it is causing unknown problems while running the gulp task package.json: moving all dependencies into devDependencies _page_committeemembers.scss: rule to hide top border on table

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ gulp.task('styles', function(){
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest('dist/styles/unminified'))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(minifycss())
+    //.pipe(minifycss())
     .pipe(livereload())
     .pipe(gulp.dest('dist/styles/minified'))
     .pipe(notify({ message: 'Styles task complete' }));
